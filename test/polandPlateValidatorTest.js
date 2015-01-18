1bed4970d17f624440b2bcc35bdf68bad82e181f
Zenedith <github@zenedith.mailgun.org>
2015-01-18 22:06:03 +0100

    Updated regex and bumped version

--- test/polandPlateValidatorTest.js
+++ test/polandPlateValidatorTest.js
@@ -16,17 +16,25 @@ describe('poland plate validator test', function () {
   });
 
   it('should validate plate in format AAA 11AA', function (done) {
-    var plate = 'pwr 17wq';
+    var plate = 'AAA 11AA';
 
     polandPlateValidator.validate(plate, function (err, data) {
       expect(err, plate + ' should be valid').is.false;
-      data.plate.should.equal('PWR17WQ');
+      data.plate.should.equal('AAA11AA');
       done();
     });
   });
 
-  it('should check if plate is valid', function (done) {
+  it('should check if plate AAA 11AA is valid', function (done) {
     var plate = 'pwr 17wq';
+
+    var result = polandPlateValidator.isValid(plate);
+    expect(result, plate + ' should be valid').is.true;
+    done();
+  });
+
+  it('should check if plate AAA 11111 is valid', function (done) {
+    var plate = 'AAA11111';
 
     var result = polandPlateValidator.isValid(plate);
     expect(result, plate + ' should be valid').is.true;
