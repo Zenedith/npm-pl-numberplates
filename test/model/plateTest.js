3469d82b63ce337fad3fa797eefefd601c5a1842
Zenedith <github@zenedith.mailgun.org>
2015-02-19 00:07:07 +0100

    Added location in plate test

--- test/model/plateTest.js
+++ test/model/plateTest.js
@@ -6,11 +6,14 @@ describe('format model test', function () {
 
   it('should create model', function (done) {
 
-    var formatModel = new plate.Plate('plate', 'prefix', 'number');
+    var formatModel = new plate.Plate('plate', 'prefix', 'number', {state: 'state', country: 'country'});
     should.exist(formatModel);
     formatModel.plate.should.be.equal('plate');
     formatModel.prefix.should.be.equal('prefix');
     formatModel.number.should.be.equal('number');
+    should.exist(formatModel.location);
+    formatModel.location.state.should.be.equal('state');
+    formatModel.location.country.should.be.equal('country');
 
     done();
   });
@@ -21,12 +24,16 @@ describe('format model test', function () {
       .withPlate('plate')
       .withPrefix('prefix')
       .withNumber('number')
+      .withLocation({state: 'state', country: 'country'})
       .build();
 
     should.exist(formatModel);
     formatModel.plate.should.be.equal('plate');
     formatModel.prefix.should.be.equal('prefix');
     formatModel.number.should.be.equal('number');
+    should.exist(formatModel.location);
+    formatModel.location.state.should.be.equal('state');
+    formatModel.location.country.should.be.equal('country');
 
     done();
   });
